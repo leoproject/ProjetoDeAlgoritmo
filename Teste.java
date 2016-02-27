2136e3aebd888ea02e8341690733d058018673c2
Leonardo de Jesus <leonardo2013ufs@gmail.com>
2016-02-27 11:58:26 -0300

    Alteração da classe teste

--- Teste.java
+++ Teste.java
@@ -1,62 +1,49 @@
- 
-///////////////////////////////////////////////////////////
-///    Teste.java
-//////////////////////////////////////////////////////////
- 
- 
 package ProjetoDeAlgoritmo;
  
 public class Teste {
  
-       public static void main(String[] args) {
-            
-             /*
-            
-             int p = 8;
-            
-             long iter = Fatorial.fatIter(p);
-            
-            
-             long ini = System.nanoTime();
-             iter = Fatorial.fatIter(p);
-             long fim = System.nanoTime();
-            
-             System.out.println(iter);
-             System.out.println(fim-ini);
-            
-             ini = System.nanoTime();
-             long rec  = Fatorial.fatRec(p);
-             fim = System.nanoTime();
-            
-             System.out.println(rec);
-             System.out.println(fim-ini);
-            
-             */
-            
-             int p = 8;
+                public static void main(String[] args) {
+                              
+                               ///*
+                               int p = 20;
+                              
+                               long iter = Fatorial.fatIter(p), rec = Fatorial.fatRec(p), ini = 0, fim = 0;
+                              
+                               ini = System.nanoTime();
+                               iter = Fatorial.fatIter(p);
+                               fim = System.nanoTime();
+                              
+                               System.out.println(iter);
+                               System.out.println(fim-ini);
+                              
+                               ini = System.nanoTime();
+                               rec  = Fatorial.fatRec(p);
+                               fim = System.nanoTime();
+                              
+                               System.out.println(rec);
+                               System.out.println(fim-ini);
+                               //*/
+                              
+                               /*
+                               int p = 30;
+                              
+                               long iter = Fibonacci.fiboIter(p), rec = Fibonacci.fiboRec(p), ini = 0, fim = 0;
+                              
+                               ini = System.nanoTime();
+                               iter = Fibonacci.fiboIter(p);
+                               fim = System.nanoTime();
+                              
+                               System.out.println(iter);
+                               System.out.println(fim-ini);
+                              
+                               ini = System.nanoTime();
+                               rec  = Fibonacci.fiboRec(p);
+                               fim = System.nanoTime();
+                              
+                               System.out.println(rec);
+                               System.out.println(fim-ini);
+                               */
  
-             long iter = Fibonacci.fiboIter(p);
-            
-            
-             long ini = System.nanoTime();
-             iter = Fibonacci.fiboIter(p);
-             long fim = System.nanoTime();
-            
-             System.out.println(iter);
-             System.out.println(fim-ini);
-            
-             ini = System.nanoTime();
-             long rec  = Fibonacci.fiboRec(p);
-             fim = System.nanoTime();
-            
-             System.out.println(rec);
-             System.out.println(fim-ini);
-            
- 
-       }
+                }
  
 }
- 
- 
-
- 
