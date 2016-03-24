0b2c1488f9ecea5305e9a8005284acb97684a0c2
Leonardo de Jesus <leonardo2013ufs@gmail.com>
2016-03-24 19:49:07 -0300

    Algoritmo Guloso com exemplo de troco de moedas, inserindo o troco ele ver quantas moedas vau ser necessárias de 50,25,10,5

--- Guloso.java
+++ Guloso.java
@@ -1,5 +1,7 @@
 package Guloso;
 
+import java.util.Scanner;
+
 public class Guloso {
 	public void trocar(int troco) {
 		int vetor[] = { 50, 25, 10, 5 };
@@ -10,15 +12,21 @@ public class Guloso {
 			quant[i] = (int) (rest / vetor[i]);
 			rest = rest % vetor[i];
 		}
+		System.out.println("***************************");
+		System.out.println("Para o troco de " + troco);
+		System.out.println("***************************");
 		for (int i = 0; i < quant.length; i++) {
-			System.out.println("Para o troco de " + troco + " temos "+ quant[i] + " moedas de " + vetor[i]);
+			System.out.println("Temos "+ quant[i] + " moedas de " + vetor[i]);
 		}
 
 	}
 
 	public static void main(String[] args) {
 		Guloso g = new Guloso();
-		g.trocar(100);
+		Scanner s = new Scanner(System.in);
+		System.out.print("Digite o troco necessário: ");
+		int troco = s.nextInt();
+		g.trocar(troco);
 	}
 
 }
